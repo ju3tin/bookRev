6bcb7f9dfcc695d326b5c110f20f12077e869294
Terri Grierson <tmgrierson@googlemail.com>
2020-07-03 12:55:48 +0000

    initial commit

---
+++ run.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/browse')
+def browse():
+    return render_template("browse.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
