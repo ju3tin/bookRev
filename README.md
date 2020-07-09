c268675576586185552e9fe92b8c2008253fad75
Terri Grierson <tmgrierson@googlemail.com>
2020-07-09 12:39:13 +0000

    completed mongo database connection

--- README.md
+++ README.md
@@ -37,3 +37,7 @@ We continually tweak and adjust this template to help give you the best experien
 --------
 
 Happy coding!
+
+book photo by thewritelife.com
+book2 photo by british-study.com
+book3 photo by thenewyorktimes.com
--- run.py
+++ run.py
@@ -1,5 +1,40 @@
+import pymongo
 import os
 from flask import Flask, render_template
+
+
+MONGO_URI = os.getenv("MONGO_URI")
+DBS_NAME = "bookRev"
+COLLECTION_NAME = "books"
+
+"""
+ This is to make a CSV file.
+
+"""
+try:
+    f=open("csv.csv", "x")
+except:
+    print("already there")
+    f=open("csv.csv", "w")
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient("mongodb+srv://pack:pack27ney@bookrev.azmgy.mongodb.net/test")
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+documents = coll.find()
+
+for doc in documents: 
+    print(doc, file=f)
+
 
 app = Flask(__name__)
 
@@ -22,6 +57,7 @@ def register():
 @app.route('/login')
 def login():
     return render_template("login.html")
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
