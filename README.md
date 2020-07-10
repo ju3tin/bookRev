290d3336c8f77b0673d5f76734c542a1509e6996
ju3tin <ju3tin@hotmail.co.uk>
2020-07-10 02:07:10 +0000

    test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <img src="https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png" style="margin: 0;">
 
-Welcome USER_NAME,
+Welcome Terri Grierson,
 
 This is the Code Institute student template for Gitpod. We have preinstalled all of the tools you need to get started. You can safely delete this README.md file, or change it for your own project.
 
@@ -37,3 +37,7 @@ We continually tweak and adjust this template to help give you the best experien
 --------
 
 Happy coding!
+
+book photo by thewritelife.com
+book2 photo by british-study.com
+book3 photo by thenewyorktimes.com
---
+++ run.py
@@ -0,0 +1,71 @@
+import pymongo 
+import os
+from flask import Flask, render_template, url_for, request, session
+
+
+MONGO_URI = os.getenv("MONGO_URI")
+DBS_NAME = "bookRev"
+COLLECTION_NAME = "books"
+
+"""
+ This is to make a CSV file.
+
+"""
+try:
+    f=open("csv.csv", "x")
+except:
+    print("already there")
+    f=open("csv.csv", "w")
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient("mongodb+srv://pack:pack27ney@bookrev.azmgy.mongodb.net/test")
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+documents = coll.find()
+
+for doc in documents: 
+    print(doc, file=f)
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/browse')
+def browse():
+    return render_template("browse.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/csv.csv')
+def csv():
+    return render_template("csv.csv")
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
